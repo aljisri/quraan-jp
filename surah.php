b0fa772fe00b2b1035c6e081ceee0e2175f9338f
aljisri <60256921+aljisri@users.noreply.github.com>
2025-10-03 07:02:53 +0400

    Update surah.php

--- surah.php
+++ surah.php
@@ -7,33 +7,31 @@
     <link rel="stylesheet" href="style.css">
 </head>
 <body>
-    <div id="page-wrapper">
-        
-        <header class="site-header">
-            <h1><a href="/">Quraan.jp</a></h1>
-        </header>
+    <header class="site-header">
+        <h1><a href="/">Quraan.jp</a></h1>
+    </header>
+    
+    <div id="main-container" class="main-container">
+        <div class="content-wrapper">
+            <header class="content-header">
+                <a href="/" class="close-button">×</a>
+                <h2 id="view-title">読み込み中...</h2>
+            </header>
 
-        <header id="content-header" class="content-header">
-            <a href="/" id="close-button" class="close-button">×</a>
-            <h2 id="view-title">読み込み中...</h2>
-        </header>
-
-        <div class="main-container">
-            
-            <main id="main-content">
+            <main>
                 <div id="ayah-view" class="view-container">
                     </div>
             </main>
-
-            <aside id="sidebar">
-                <div class="sidebar-header">
-                    <h3>Resources</h3>
-                </div>
-                <div id="sidebar-content">
-                    <p>アーヤをクリックすると、ここに注釈などが表示されます。</p>
-                </div>
-            </aside>
         </div>
+
+        <aside id="sidebar">
+            <div class="sidebar-header">
+                <h3>Resources</h3>
+            </div>
+            <div id="sidebar-content">
+                <p>アーヤをクリックすると、ここに注釈などが表示されます。</p>
+            </div>
+        </aside>
     </div>
 
     <script src="surah.js"></script>
