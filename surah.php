789dd110648793d60e923e84a0b1d0abca647a32
aljisri <60256921+aljisri@users.noreply.github.com>
2025-10-03 07:40:07 +0400

    Update surah.php

--- surah.php
+++ surah.php
@@ -6,23 +6,22 @@
     <title>Quraan.jp</title>
     <link rel="stylesheet" href="style.css">
 </head>
-<body>
-    <header class="site-header">
+<body> <header class="site-header">
         <h1><a href="/">Quraan.jp</a></h1>
     </header>
-    
-    <div id="page-container" class="page-container">
-        
-        <main id="main-content">
-            <div class="view-container">
-                <header class="content-header">
-                    <a href="/" class="close-button">×</a>
-                    <h2 id="view-title">読み込み中...</h2>
-                </header>
-                <div id="ayah-view">
+
+    <div class="page-container">
+        <div class="main-content-wrapper">
+            <header class="content-header">
+                <a href="/" class="close-button">×</a>
+                <h2 id="view-title">読み込み中...</h2>
+            </header>
+
+            <main>
+                <div id="ayah-view" class="view-container">
                     </div>
-            </div>
-        </main>
+            </main>
+        </div>
 
         <aside id="sidebar">
             <div class="sidebar-header">
