dd3496403a166e438cd41dceac8c7196ec644767
aljisri <60256921+aljisri@users.noreply.github.com>
2025-10-03 16:44:05 +0400

    Update surah.php

--- surah.php
+++ surah.php
@@ -10,33 +10,26 @@
     <header class="site-header">
         <h1><a href="/">Quraan.jp</a></h1>
     </header>
-    
-    <div id="page-container" class="page-container">
+    <div class="page-container">
         <div class="main-content-wrapper">
             <header class="content-header">
-                <div class="header-inner-container">
-                    <a href="/" class="close-button">×</a>
-                    <h2 id="view-title">読み込み中...</h2>
-                </div>
+                <a href="/" class="close-button">×</a>
+                <h2 id="view-title">読み込み中...</h2>
             </header>
-
             <main>
                 <div id="ayah-view" class="view-container">
                     </div>
             </main>
         </div>
-
         <aside id="sidebar">
             <div class="sidebar-header">
                 <h3>Resources</h3>
-                <button id="sidebar-close-button" class="close-button">×</button>
             </div>
             <div id="sidebar-content">
                 <p>アーヤをクリックすると、ここに注釈などが表示されます。</p>
             </div>
         </aside>
     </div>
-
     <script src="surah.js"></script>
 </body>
 </html>
